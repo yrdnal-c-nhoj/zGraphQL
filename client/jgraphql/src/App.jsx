acded1867b65096e4c972c32829699d5e36bb4ae
john <johnclandry@yahoo.com>
2025-01-26 23:14:32 -0500

    x

--- client/jgraphql/src/App.jsx
+++ client/jgraphql/src/App.jsx
@@ -1,19 +1,36 @@
-import './App.css'
-import { useQuery, useMutation, gql } from '@apollo/client'
+import './App.css';
+import { useQuery, gql } from '@apollo/client';
 
+// gql converts info from strings to syntax notation
 const GET_USERS = gql`
   query GetUsers {
-    users {
+    getusers {
       id
       name
       email
+    }
+  }
+`;
 
 function App() {
-  const { } = useQuery()
-  return <>
-    <h1>Users</h1>
-    <div></div>
-  </>;
+  const { data, error, loading } = useQuery(GET_USERS);
+
+  if (loading) return <p>Data Loading ...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <>
+      <h1>Users</h1>
+      <div>
+        {data.getusers.map((user) => (
+          <div key={user.id}>
+            <p>NAME: {user.name}</p>
+            <p>EMAIL: {user.email}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- client/jgraphql/src/main.jsx
+++ client/jgraphql/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://localhost:5173/graphql',
   cache: new InMemoryCache(),
 });
 
